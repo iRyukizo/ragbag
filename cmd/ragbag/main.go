36e911847ad3fbd1cd40f0de5b3dc4488e7c6320
Hugo Moreau <hugo.moreau@epita.fr>
2024-04-21 01:13:16 +0800

    cmd/ragbag/main.go: init simple binary

--- cmd/ragbag/main.go
+++ cmd/ragbag/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+var (
+	debug    bool
+	rom_path string
+)
+
+func init() {
+	flag.BoolVar(&debug, "debug", false, "Allow debug mode.")
+
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr, "Usage: ragbag [options] [file]")
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+
+	if len(flag.Args()) < 1 {
+		fmt.Fprintln(os.Stderr, "file is mandatory")
+		flag.Usage()
+		os.Exit(2)
+	}
+	if len(flag.Args()) > 1 {
+		fmt.Fprintln(os.Stderr, "multiple files is not handled")
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	rom_path = flag.Arg(0)
+}
 
 func main() {
-	fmt.Println("ragbag")
+	fmt.Println("ragbag", rom_path)
 }
