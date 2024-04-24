e4dc5c357a84897b6a1d2631e62f8ef6f8984e38
Hugo Moreau <hugo.moreau@epita.fr>
2024-04-24 17:17:53 +0800

    pkg/memory/region: update const formating

--- pkg/memory/region/region.go
+++ pkg/memory/region/region.go
@@ -1,8 +1,7 @@
 package region
 
-// Those const represents the GBA Memory Map.
+// Those const represent the GBA General Internal Memory Map.
 const (
-	// General Internal Memory
 	BIOSBegin        uint = 0x0000_0000
 	BIOSEnd               = 0x0000_3FFF
 	Unused1Begin          = 0x0000_4000
@@ -19,31 +18,40 @@ const (
 	IORegistersEnd        = 0x0400_03FE
 	Unused4Begin          = 0x0400_0400
 	Unused4End            = 0x04FF_FFFF
-	// Internal Display Memory
-	PaletteRAMBegin = 0x0500_0000
-	PaletteRAMEnd   = 0x0500_03FF
-	Unused5Begin    = 0x0500_0400
-	Unused5End      = 0x05FF_FFFF
-	VideoRAMBegin   = 0x0600_0000
-	VideoRAMEmd     = 0x0601_7FFF
-	Unused6Begin    = 0x0601_8000
-	Unused6End      = 0x06FF_FFFF
-	OAMBegin        = 0x0700_0000
-	OAMEnd          = 0x0700_03FF
-	Unused7Begin    = 0x0700_0400
-	Unused7End      = 0x07FF_FFFF
-	// External Memory (Game Pak)
-	GPROM0Begin  = 0x0800_0000
-	GPROM0End    = 0x09FF_FFFF
-	GPROM1Begin  = 0x0A00_0000
-	GPROM1End    = 0x0BFF_FFFF
-	GPROM2Begin  = 0x0C00_0000
-	GPROM2End    = 0x0DFF_FFFF
-	GPSRAMBegin  = 0x0E00_0000
-	GPSRAMEnd    = 0x0E00_FFFF
-	Unused8Begin = 0x0E01_0000
-	Unused8End   = 0x0FFF_FFFF
-	// Unused Memory Area
-	Unused9Begin = 0x1000_0000
-	Unused9End   = 0xFFFF_FFFF
+)
+
+// Those const represent the GBA Internal Display Memory Map.
+const (
+	PaletteRAMBegin uint = 0x0500_0000
+	PaletteRAMEnd        = 0x0500_03FF
+	Unused5Begin         = 0x0500_0400
+	Unused5End           = 0x05FF_FFFF
+	VideoRAMBegin        = 0x0600_0000
+	VideoRAMEmd          = 0x0601_7FFF
+	Unused6Begin         = 0x0601_8000
+	Unused6End           = 0x06FF_FFFF
+	OAMBegin             = 0x0700_0000
+	OAMEnd               = 0x0700_03FF
+	Unused7Begin         = 0x0700_0400
+	Unused7End           = 0x07FF_FFFF
+)
+
+// Those const represent the GBA External Memory (Game Pak) Map.
+const (
+	GPROM0Begin  uint = 0x0800_0000
+	GPROM0End         = 0x09FF_FFFF
+	GPROM1Begin       = 0x0A00_0000
+	GPROM1End         = 0x0BFF_FFFF
+	GPROM2Begin       = 0x0C00_0000
+	GPROM2End         = 0x0DFF_FFFF
+	GPSRAMBegin       = 0x0E00_0000
+	GPSRAMEnd         = 0x0E00_FFFF
+	Unused8Begin      = 0x0E01_0000
+	Unused8End        = 0x0FFF_FFFF
+)
+
+// Those const represent the GBA Unused Memory Map.
+const (
+	Unused9Begin uint = 0x1000_0000
+	Unused9End        = 0xFFFF_FFFF
 )
